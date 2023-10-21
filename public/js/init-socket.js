25fc015f1712d3e1f2176b3dd9e71def421c6f83
janmas <moyuucat@gmail.com>
2023-10-21 17:39:16 +0800

    websocket前端搭建

--- public/js/init-socket.js
+++ public/js/init-socket.js
@@ -2,8 +2,25 @@ const query = new URLSearchParams(window.location.search);
 const room = query.get('room');
 options.urlParam = query.getAll();
 const drawWebsocket = {
+    websocketInstance: null,
+    options: {
+        url: '',
+        urlParam: {},
+        pingTime: 10,//心跳间隔
+        timeout: 5,//超时时间
+    },
     init(options) {
+        this.options.extend({}, this.options, options)
         //检测是否支持websocket
-    }
+        if (!window.WebSocket) {
+            alert('您的浏览器不支持websocket无法参与协作');
+            return;
+        }
+        let url = this.buildUrl()
+        this.websocketInstance = new WebSocket(this.options.url)
+    },
+    buildurl() {
+        let url = this.options.url
+    },
 }
 
